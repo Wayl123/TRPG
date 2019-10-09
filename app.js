c1920fb6823299e1229753f1914f717f3c8c9cbd
Wayne Lee <waynel1996weiyang@gmail.com>
2019-10-08 21:31:03 -0400

    Change pathing to be redirected from landing page. Modify index route. Modify to contain new route and create route.

--- app.js
+++ app.js
@@ -1,8 +1,49 @@
 var express = require("express"),
-	app = express();
-	
+	app = express(),
+	bodyParser = require("body-parser"),
+	mongoose = require("mongoose"),
+	//models
+	Character = require("./models/character"),
+	Skill = require("./models/skill"),
+	Weapon = require("./models/weapon");
+
+const url = process.env.DATABASEURL || "mongodb://localhost:27017/trpg";
+mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.set("view engine", "ejs");
+
+//landing
 app.get("/", (req, res) => {
-	res.render("index");
+	res.redirect("/trpg");
+});
+
+//trpg
+//index
+app.get("/trpg", (req, res) => {
+	Character.find({}, (err, characters) => {
+		if(err){
+			console.log(err);
+		} else {
+			res.render("index", {characters: characters});
+		}
+	});
+});
+
+//new
+app.get("/trpg/new", (req, res) => {
+	res.render("new");
+});
+
+//create
+app.post("/trpg", (req, res) => {
+	Character.create(req.body.character, (err, character) => {
+		if(err){
+			console.log(err);
+		} else {
+			res.redirect("/trpg");
+		}
+	});
 });
 	
 const PORT = process.env.PORT || 3000;
