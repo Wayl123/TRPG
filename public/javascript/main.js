f2dfd87fda86d7e76b846e0c2d647aa7fac4d821
Wayne Lee <waynel1996weiyang@gmail.com>
2019-10-28 00:07:16 -0400

    Allow modification of stat in edit page. Comment out job delete option for now.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,6 +1,7 @@
-//delete job from character
-$(".deleteJob").click((event) => {
-	character.jobs.splice(character.jobs.findIndex(job => job.name === this.name), 1);
-	$(this).parent().remove();
-	event.stopPropagation();
-});
+//delete job from character, will do this later maybe
+// <span class="btn btn-sm btn-danger deleteJob">Remove</span>
+// $(".deleteJob").click((event) => {
+	// character.jobs.splice(character.jobs.findIndex(job => job.name === this.name), 1);
+	// $(this).parent().remove();
+	// event.stopPropagation();
+// });
