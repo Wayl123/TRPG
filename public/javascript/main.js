495d3db3149c418d1d4499f667b93e59e7bfc3c0
Wayne Lee <waynel1996weiyang@gmail.com>
2019-11-07 03:21:03 -0500

    Add battle page with basic stuff. I didn't know that arrow function doesn't set this keyword properly though.

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -1,1 +1,5 @@
-
+$(".selectSkill").change(function() {
+	var skill = $(this).find(":selected").val().split("/");
+	var out = "Type: " + skill[0] + "<br>Accuracy: " + skill[1] + "<br>Damage: " + skill[3] + "-" + skill[2] + "<br>Rolls: " + skill[4];
+	$(this).parent().children(".skillOut").html(out);
+});
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require("express"),
-	router = express.Router();
+	router = express.Router(),
+	//models
+	Character = require("../models/character");
 	
 //landing
 router.get("/", (req, res) => {
 	res.redirect("/trpg");
+});
+
+router.get("/battle", (req, res) => {
+	Character.find({}).
+	populate([{
+		path: "jobs",
+		populate: {path: "skills"}
+	},
+	{
+		path: "weapon",
+		populate: {path: "skills"}
+	}]).exec((err, characters) => {
+		if(err){
+			console.log(err);
+		} else {
+			res.render("battle", {characters: characters});
+		}
+	});
 });
 	
 module.exports = router;
